b2053be0c3c0fca3f04fbf51004489a03eca3753
Luis Guilherme Polippo <luispolippo@hotmail.com>
2021-03-08 00:05:23 -0300

    created options menu exemple

--- app/src/main/java/com/polippo/contactlist/MainActivity.kt
+++ app/src/main/java/com/polippo/contactlist/MainActivity.kt
@@ -2,6 +2,10 @@ package com.polippo.contactlist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -39,5 +43,29 @@ class MainActivity : AppCompatActivity() {
                 )
             )
         )
+    }
+
+    private fun showToast(message: String){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.item_menu_1 ->{
+                showToast("Exibindo item de menu 1")
+                true
+            }
+            R.id.item_menu_2 ->{
+                showToast("Exibindo item de menu 2")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
