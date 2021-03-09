b567ad30f9bbe1431da47bcd209d78e83ec61323
Luis Guilherme Polippo <luispolippo@hotmail.com>
2021-03-08 22:33:31 -0300

    implemented SharedPreferences

--- app/src/main/java/com/polippo/contactlist/MainActivity.kt
+++ app/src/main/java/com/polippo/contactlist/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.polippo.contactlist
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -10,9 +12,12 @@ import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.edit
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import com.polippo.contactlist.DetailActivity.Companion.EXTRA_CONTACT
 
 class MainActivity : AppCompatActivity(), ClickItemContactListener {
@@ -26,8 +31,33 @@ class MainActivity : AppCompatActivity(), ClickItemContactListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.drawer_menu)
         initDrawer()
+        fetchListContact()
         bindView()
-        updateList()
+
+    }
+
+    private fun fetchListContact(){
+        val list = arrayListOf(
+                Contact(
+                        "Luis Polippo",
+                        "(00) 0000-0000",
+                        "img.png"
+                ),
+                Contact(
+                        "Guilherme Correa",
+                        "(00) 9999-9999",
+                        "img.png"
+                )
+        )
+
+        getInstanceSharedPreferences().edit {
+            putString("contacts", Gson().toJson(list))
+            commit()
+        }
+    }
+
+    private fun getInstanceSharedPreferences():SharedPreferences{
+        return getSharedPreferences("com.polippo.contactlist.PREFERENCES", Context.MODE_PRIVATE)
     }
 
     private fun initDrawer(){
@@ -43,23 +73,17 @@ class MainActivity : AppCompatActivity(), ClickItemContactListener {
     private fun bindView(){
         rvList.adapter = adpter
         rvList.layoutManager = LinearLayoutManager(this)
+        updateList()
+    }
+
+    private fun getListContacts(): List<Contact>{
+        val list = getInstanceSharedPreferences().getString("contacts", "[]")
+        val turnsType = object: TypeToken<List<Contact>>() {}.type
+        return Gson().fromJson(list, turnsType)
     }
 
     private fun updateList(){
-        adpter.updateList(
-            arrayListOf(
-                Contact(
-                    "Luis Polippo",
-                    "(00) 0000-0000",
-                    "img.png"
-                ),
-                Contact(
-                    "Guilherme Correa",
-                    "(00) 9999-9999",
-                    "img.png"
-                )
-            )
-        )
+        adpter.updateList(getListContacts())
     }
 
     private fun showToast(message: String){
